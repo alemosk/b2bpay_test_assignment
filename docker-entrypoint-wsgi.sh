f91430f3e7d097a23ac0a1ea156050cb76dea2dc
Aleksei Baranov <aleksei.ee@gmail.com>
2024-11-08 03:06:28 +0200

    TA: tests

--- docker-entrypoint-wsgi.sh
+++ docker-entrypoint-wsgi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # python manage.py collectstatic --no-input
-# create db
+
 bash ./deployment/wsgi/wait-for-db.sh
+
+pytest
+
 alembic upgrade head
+
 gunicorn b2bpay.config.wsgi:application --bind 0.0.0.0:8080 --preload --timeout 90 -w 4
