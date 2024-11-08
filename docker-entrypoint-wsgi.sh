2149c1e47f70fd7e98e581be62557868fd933ec1
Aleksei Baranov <aleksei.ee@gmail.com>
2024-11-08 03:08:18 +0200

    TA: comments

--- docker-entrypoint-wsgi.sh
+++ docker-entrypoint-wsgi.sh
@@ -4,8 +4,10 @@
 
 bash ./deployment/wsgi/wait-for-db.sh
 
+# run tests (only applicable for test assigment)
 pytest
 
+# migrations
 alembic upgrade head
 
 gunicorn b2bpay.config.wsgi:application --bind 0.0.0.0:8080 --preload --timeout 90 -w 4
